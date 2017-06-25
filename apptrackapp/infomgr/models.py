fc8ccecf968e8ebddbed22569beaccf3acf30700
Chintan Panchamia <chintan10.panchamia@gmail.com>
2017-06-25 17:00:37 -0400

    update models and form

--- apptrackapp/infomgr/models.py
+++ apptrackapp/infomgr/models.py
@@ -11,8 +11,14 @@ class UserInfo(models.Model):
     middle_name = models.CharField(max_length=30, blank=True)
     last_name = models.CharField(max_length=50, default='Doe')
 
+    def __str__(self):
+        return "{} {}".format(self.first_name, self.last_name)
+
+
+
+class Profile(models.Model):
+    userinfo = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
     email = models.EmailField(max_length=50, default='abc@def.com')
-    # user = models.OneToOneField(User, on_delete=models.CASCADE) #FName, LName Username, Email, Password
 
     phone_regex = RegexValidator(regex=r'^\+?1?\d{10,15}$', message="Phone number must be entered in the format: '+9999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], blank=True, max_length=15) # validators should be a list
@@ -21,18 +27,13 @@ class UserInfo(models.Model):
     location = models.CharField(max_length=50, blank=True)
     zipcode = models.CharField(max_length=5, blank=True)
 
+    profile = models.CharField(max_length=50, default='Full Stack Developer')
+    skills = models.TextField()
     summary = models.TextField(blank=True)
     cover_letter = models.TextField(blank=True)
     resume = models.TextField()
 
-    def __str__(self):
-        return "{} {}".format(self.first_name, self.last_name)
-
-    # @receiver(post_save, sender=User)
-    # def create_user_profile(sender, instance, created, **kwargs):
-    #     if created:
-    #         UserInfo.objects.create(user=instance)
-    #
-    # @receiver(post_save, sender=User)
-    # def save_user_profile(sender, instance, **kwargs):
-    #     instance.userinfo.save()
+    github_link = models.TextField(blank=True)
+    linkedin_link = models.TextField(blank=True)
+    twitter_link = models.TextField(blank=True)
+    portfolio_link = models.TextField(blank=True)
--- apptrackapp/infomgr/urls.py
+++ apptrackapp/infomgr/urls.py
@@ -4,5 +4,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^$', views.index, name='index'),
+    url(r'^register$', views.UserFormView.as_view, name='register')
 ]
